6f7d144521149d26268286845486537cf2b1ec4b
ishita1805 <ishitakabra1805@gmail.com>
2025-02-06 01:43:33 +0530

    fix: remove tsx extensions from imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import App from './App.tsx'
+import App from './App'
 import './index.css'
-import Collection from './pages/Collection.tsx';
-import Design from './pages/Design.tsx';
-import Product from './pages/Product.tsx';
-import Cart from './pages/Cart.tsx';
-import Login from './pages/Login.tsx';
-import Privacy from './pages/Privacy.tsx';
-import Wishlist from './pages/Wishlist.tsx';
+import Collection from './pages/Collection';
+import Design from './pages/Design';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Privacy from './pages/Privacy';
+import Wishlist from './pages/Wishlist';
 import { DBProvider } from "./context"
-import ScrollWrapper from './hooks/scrollToTop.tsx';
-import RedirectRoutes from './components/RedirectRoutes.tsx';
+import ScrollWrapper from './hooks/scrollToTop';
+import RedirectRoutes from './components/RedirectRoutes';
 import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')!).render(
