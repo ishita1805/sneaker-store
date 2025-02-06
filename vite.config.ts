0d84be13c2ccc9c019b5201406be524293998053
ishita1805 <ishitakabra1805@gmail.com>
2025-02-06 16:00:52 +0530

    feat:gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sneaker-store',
   css: {
     postcss: {
       plugins: [tailwindcss()],
